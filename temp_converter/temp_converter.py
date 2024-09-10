bafc188acb46bf95dea7ab7eb1ea253e09110153
sudoCreate258 <59755293+sudoCreate258@users.noreply.github.com>
2024-09-10 14:47:42 -0400

    Update temp_converter.py
    added f2c_op

--- temp_converter/temp_converter.py
+++ temp_converter/temp_converter.py
@@ -3,12 +3,16 @@
 # process
 #def c2f(c):
 #    return c * 9 / 5 + 32
-def f2c(f):
+def f2c_raw(f):
     return f - 32 * 5 / 9
 
-def main(fah):
-    return f2c(fah)
+def f2c_op(f):
+    return f - 32 * 5 / 9
+
+def main():
+    f = 0           # input
+    c = f2c(f)
+    print(f"{f}F is {c} C")
 
 if __name__ == "__main__":
-    f = 0           # input
-    print(main(f))  # output
+    main()
